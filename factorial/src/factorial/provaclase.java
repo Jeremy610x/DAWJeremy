ee0e342233c736e04c82d4ff5780ad2cae808afc
Jeremy Panchana Cirino <jepa2698@proven.local>
2022-11-22 14:43:11 +0100

    commit

---
+++ factorial/src/factorial/provaclase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factorial;
+
+/**
+ *
+ * @author jepa2698
+ */
+public class provaclase {
+    public static void main(String[] args) {
+        
+    }
+    
+}
