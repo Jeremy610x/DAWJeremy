554e09bbac25ef396332525b9ec04f1d580cd8a5
Jeremy Panchana Cirino <jepa2698@proven.local>
2022-11-23 13:30:25 +0100

    commit

--- NF2/src/nf2/exercici_clase_23112022.java
+++ NF2/src/nf2/exercici_clase_23112022.java
@@ -13,24 +13,34 @@ public class exercici_clase_23112022 {
     public static void main(String[] args) {
         //definicio arrays
         int [] notes;
-        int num_notas=7;
+        int num_notas=4;
         
         
         notes = new int[num_notas];
         notes[0]=3;
         notes[1]=4;
         notes[2]=7;
-        notes[5]=1;
+        notes[3]=1;
         
-        System.out.println("Array creat" +notes);
         
-        System.out.println("Mostrar posicions array");
         for(int i=0;i < notes.length; i++){
             System.out.println("Posicio " + i+" conte - " +notes[i]);
         }
+            
+        int suma=0;
+        double media;
+        
+       
+        for(int i = 0; i < notes.length; i++) {
+            System.out.println("Posicio " + i+" conte - " +notes[i]);
+            suma= suma + notes[i];
+            System.out.println("Acumulado por ahora" + suma);
+        }
+        }
         
         
-    }
+        
+    
     
     
     
