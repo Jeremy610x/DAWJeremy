9f161e7a28e7f74f62425e2ddf614fb83dd58104
Jeremy <geovannyjp332@gmail.com>
2022-11-23 19:49:15 +0100

    commit fet a casa

---
+++ JavaFet_a_Casa/src/javafet_a_casa/JavaFet_a_Casa.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javafet_a_casa;
+
+/**
+ *
+ * @author geova
+ */
+public class JavaFet_a_Casa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Miquel ha guanyat Espanya 7-0 a Costa Rica el dia 23-11-2022");
+        
+    }
+    
+}
